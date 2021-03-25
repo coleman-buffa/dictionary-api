16992766a39e9a208da88de350ad665d18795dd2
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-25 12:41:15 -0700

    Create route that returns all words that start with a given letter.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,29 +4,41 @@ const Dictionary = require("../models/dictionary.js");
 //Retrieve all words
 router.get("/api/words", (req, res) => {
   Dictionary.find({})
-  .select("word")
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => {
-    res.json(data)
-  });
+    .select("word")
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err)
+    });
 });
 
 //Retrieve meaning of a given word
-router.get("/api/words/:word", (req, res) => {
+router.get("/api/word/:word", (req, res) => {
   Dictionary.findOne({
     word: req.params.word
   })
-  .select("meaning")
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => {
-    res.json(data)
-  });
+    .select("meaning")
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err)
+    });
 });
 
 //List all the words starting with a given letter
+router.get("/api/words/:letter", (req, res) => {
+  Dictionary.find({
+    word: { $regex: '^' + req.params.letter, $options: 'i' }
+  })
+    .select("word")
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err)
+    });
+});
 
 module.exports = router;
