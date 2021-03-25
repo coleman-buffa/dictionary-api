5c564021513146bff22e9c3970f3d0d28c7fc8f0
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-25 12:07:29 -0700

    Create route that returns all words.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const Dictionary = require("../models/dictionary.js");
 
 //Retrieve all words
+router.get("/api/words", (req, res) => {
+  Dictionary.find({})
+  .select("word")
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.json(data)
+  });
+});
 
 //Retrieve meaning of a given word
 
