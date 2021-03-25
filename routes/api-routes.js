9bb73289508fa9b74a081e136c592a328809ba07
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-25 11:41:44 -0700

    Create stubs for initial set of routes.

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const Dictionary = require("../models/dictionary.js");
+
+//Retrieve all words
+
+//Retrieve meaning of a given word
+
+//List all the words starting with a given letter
+
+module.exports = router;
