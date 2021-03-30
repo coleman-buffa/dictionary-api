0020d0f694f4d646995c92c5970053f8e84654c3
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-30 14:22:17 -0700

    Initialize react files and enter starter meta data.

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Connect to MongoDB instance
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dictionaryDB", { useNewUrlParser: true, useFindAndModify: false });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Register API router
 app.use(require("./routes/api-routes.js"));
 
+//Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
